a7627b5901ddc3889395063a80dd7a591381b986
Jørgen Traasdahl Fornes <jorgetf@n017.cm.cluster>
2025-10-27 12:26:53 +0100

    chnaged code to compatible with doca version 2.7.0

--- dma_transfer/dma_copy_dpu.c
+++ dma_transfer/dma_copy_dpu.c
@@ -1,97 +1,174 @@
-#include <doca_mmap.h>
-#include <doca_dma.h>
-#include <doca_comch.h>
+// dma_dpu.c
+// Build with DOCA 2.7 libs. DPU side: create a local buffer with test data,
+// connect to host server, receive export descriptor, create remote map of host buffer,
+// submit DOCA DMA copy from local DPU buffer -> remote host buffer, wait completion,
+// send "DONE" back to host.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
 
-#include "dma_copy.h"
+#include <doca_mmap.h>
+#include <doca_buffer.h>
+#include <doca_dev.h>
+#include <doca_dma.h>
+#include <doca_pe.h>
+#include <doca_error.h>
+
+#define HOST_IP getenv("HOST_IP")  // <--- replace with actual Host IP reachable from DPU
+#define HOST_PORT 8888
+#define BUF_SIZE 4096
+#define EXPORT_MAX 65536
+
+static void die(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv) {
-    // 1. Parse args: PCIe addresses (-p for comm channel, -r for representor on DPU), file size etc.
-    doca_comch_ep_t *dpu_ep = NULL;
-    char *pcie_addr = getenv("PCIe_ADDR");
-    char *rep_pcie_addr = getenv("REP_PCIe_ADDR");
-    doca_comch_ep_listen(pcie_addr, rep_pcie_addr, &dpu_ep);
+    int sock = -1;
+    ssize_t r;
+    uint8_t *recv_desc = malloc(EXPORT_MAX);
+    size_t recv_desc_len = 0;
+    uint32_t net_len = 0;
 
-    // 2. Receive host export descriptor + host buffer address + size
-    size_t export_desc_size = BUFSIZE;  // maybe send size first
-    uint8_t *export_desc = malloc(export_desc_size);
-    doca_comch_recv(dpu_ep, export_desc, export_desc_size, 0);
+    // 1) Prepare a DPU local buffer and fill it with sample data
+    void *dpu_buf = NULL;
+    if (posix_memalign(&dpu_buf, sysconf(_SC_PAGESIZE), BUF_SIZE) != 0) die("posix_memalign");
+    for (int i = 0; i < BUF_SIZE; ++i) ((unsigned char*)dpu_buf)[i] = (unsigned char)(i & 0xff);
 
-    void *host_buf_addr = NULL;
-    size_t host_buf_size = 0;
-    doca_comch_recv(dpu_ep, &host_buf_addr, sizeof(host_buf_addr), 0);
-    doca_comch_recv(dpu_ep, &host_buf_size, sizeof(host_buf_size), 0);
-
-    // 3. Map host buffer as remote DOCA buffer
-    doca_mmap_t *host_mmap = NULL;
-    doca_mmap_create_from_export(export_desc, export_desc_size,
-                                 /*dev*/, &host_mmap);
-    doca_buffer_t *buf_host = NULL;
-    doca_buffer_from_mmap(host_mmap, host_buf_addr,
-                          host_buf_size, &buf_host);
-
-    // 4. Allocate DPU local buffer (source of data)
-    size_t dpu_buf_size = host_buf_size;
-    void *dpu_buf_ptr = malloc(dpu_buf_size);
-    // fill data:
-    fill_data(dpu_buf_ptr, dpu_buf_size);
-
+    // Create DOCA mmap for local DPU buffer (so we can create doca_buffer)
     doca_mmap_t *dpu_mmap = NULL;
-    doca_mmap_create(&dpu_mmap);
-    doca_mmap_set_memrange(dpu_mmap, dpu_buf_ptr, dpu_buf_size);
+    if (doca_mmap_create(&dpu_mmap) != 0) { fprintf(stderr,"doca_mmap_create dpu failed\n"); return -1; }
+    doca_mmap_set_memrange(dpu_mmap, dpu_buf, BUF_SIZE);
     doca_mmap_set_permissions(dpu_mmap, DOCA_ACCESS_FLAG_PCI_READ_WRITE);
-    doca_mmap_start(dpu_mmap);
-
+    if (doca_mmap_start(dpu_mmap) != 0) { fprintf(stderr,"doca_mmap_start dpu failed\n"); return -1; }
     doca_buffer_t *buf_dpu = NULL;
-    doca_buffer_from_mmap(dpu_mmap, dpu_buf_ptr,
-                          dpu_buf_size, &buf_dpu);
+    if (doca_buffer_from_mmap(dpu_mmap, dpu_buf, BUF_SIZE, &buf_dpu) != 0) { fprintf(stderr,"doca_buffer_from_mmap dpu failed\n"); return -1; }
 
-    // 5. Setup DMA device/context
-    doca_dev_t *dev = NULL;
-    open_dma_device(/* PCIe addr */, &dev);
-    doca_dma_t *dma = NULL;
-    doca_dma_create(dev, &dma);
-    doca_ctx_t *ctx = doca_dma_as_ctx(dma);
-    doca_pe_t *pe = NULL;
-    doca_pe_create(&pe);
-    doca_pe_connect_ctx(pe, ctx);
-    doca_ctx_start(ctx);
+    // 2) Connect to host server (DPU initiates TCP)
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) die("socket");
 
-    // 6. Issue DMA copy task: from buf_dpu → buf_host
-    doca_dma_task_memcpy *memcpy_task = NULL;
-    doca_dma_task_memcpy_alloc_init(dma,
-                                    buf_dpu,
-                                    buf_host,
-                                    /* user_data */0,
-                                    &memcpy_task);
-
-    doca_task_t *task = doca_dma_task_memcpy_as_task(memcpy_task);
-    doca_task_submit(task);
-
-    // Wait for completion
-    while (!task_done) {
-        doca_pe_progress(pe);
+    struct sockaddr_in servaddr = {0};
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(HOST_PORT);
+    if (inet_pton(AF_INET, HOST_IP, &servaddr.sin_addr) != 1) {
+        fprintf(stderr, "invalid HOST_IP\n");
+        return -1;
     }
 
+    printf("DPU: connecting to host %s:%d...\n", HOST_IP, HOST_PORT);
+    if (connect(sock, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) die("connect");
+    printf("DPU: connected\n");
+
+    // 3) Receive export descriptor length, descriptor, host ptr and size
+    if ((r = recv(sock, &net_len, sizeof(net_len), MSG_WAITALL)) != sizeof(net_len)) {
+        die("recv net_len");
+    }
+    recv_desc_len = ntohl(net_len);
+    if (recv_desc_len > EXPORT_MAX) {
+        fprintf(stderr, "export descriptor too big\n");
+        return -1;
+    }
+
+    if ((r = recv(sock, recv_desc, recv_desc_len, MSG_WAITALL)) != (ssize_t)recv_desc_len) die("recv desc");
+    printf("DPU: received export descriptor (%zu bytes)\n", recv_desc_len);
+
+    uint64_t host_ptr_net = 0;
+    uint64_t host_size_net = 0;
+    if (recv(sock, &host_ptr_net, sizeof(host_ptr_net), MSG_WAITALL) != sizeof(host_ptr_net)) die("recv host ptr");
+    if (recv(sock, &host_size_net, sizeof(host_size_net), MSG_WAITALL) != sizeof(host_size_net)) die("recv host size");
+    uint64_t host_ptr = be64toh(host_ptr_net);
+    uint64_t host_size = be64toh(host_size_net);
+    printf("DPU: host_ptr=0x%lx, host_size=%lu\n", (unsigned long)host_ptr, (unsigned long)host_size);
+
+    // 4) Create mmap from export (import host exported region)
+    doca_mmap_t *host_mmap = NULL;
+    if (doca_mmap_create_from_export(recv_desc, recv_desc_len, /* dev */ NULL, &host_mmap) != 0) {
+        fprintf(stderr,"doca_mmap_create_from_export failed\n");
+        return -1;
+    }
+
+    doca_buffer_t *buf_host_remote = NULL;
+    if (doca_buffer_from_mmap(host_mmap, (void*) (uintptr_t)host_ptr, host_size, &buf_host_remote) != 0) {
+        fprintf(stderr,"doca_buffer_from_mmap (host remote) failed\n");
+        return -1;
+    }
+
+    // 5) Setup DOCA DMA on DPU side: open device, create dma, submit memcpy buf_dpu -> buf_host_remote
+    doca_dev_t *dev = NULL;
+    if (doca_dev_open(0, &dev) != 0) {
+        fprintf(stderr,"doca_dev_open failed\n");
+        return -1;
+    }
+
+    doca_dma_t *dma = NULL;
+    if (doca_dma_create(dev, &dma) != 0) {
+        fprintf(stderr,"doca_dma_create failed\n");
+        return -1;
+    }
+
+    // Create memcpy task
+    doca_dma_task_memcpy *memcpy_task = NULL;
+    if (doca_dma_task_memcpy_alloc_init(dma,
+                                       buf_dpu,
+                                       buf_host_remote,
+                                       0, /* user_data */
+                                       &memcpy_task) != 0) {
+        fprintf(stderr,"doca_dma_task_memcpy_alloc_init failed\n");
+        return -1;
+    }
+
+    doca_task_t *task = doca_dma_task_memcpy_as_task(memcpy_task);
+    if (!task) { fprintf(stderr,"task creation failed\n"); return -1; }
+
+    printf("DPU: submitting DMA memcpy task (DPU->Host)\n");
+    if (doca_task_submit(task) != 0) {
+        fprintf(stderr,"doca_task_submit failed\n");
+        return -1;
+    }
+
+    // 6) Poll/drive progress until complete
+    doca_pe_t *pe = NULL;
+    doca_pe_create(&pe);
+    doca_pe_connect_ctx(pe, doca_dma_as_ctx(dma));
+    int completed = 0;
+    while (!completed) {
+        doca_pe_progress(pe);
+        // doca_task_poll / check status: simplified - check task state
+        if (doca_task_get_status(task) == DOCA_TASK_STATE_FINISHED) {
+            completed = 1;
+            break;
+        }
+        usleep(1000);
+    }
+    printf("DPU: DMA task completed\n");
+
+    // free task
     doca_task_free(task);
 
-    // 7. Signal host side done
-    int done = 1;
-    doca_comch_send(dpu_ep, &done, sizeof(done), 0);
+    // 7) Send "DONE" back to Host
+    if (send(sock, "DONE", 4, 0) != 4) perror("send DONE");
+    printf("DPU: sent DONE to host\n");
 
-    // 8. Cleanup
-    doca_buffer_destroy(buf_dpu);
-    doca_buffer_destroy(buf_host);
-    doca_mmap_destroy(dpu_mmap);
-    doca_mmap_destroy(host_mmap);
-    free(dpu_buf_ptr);
-    free(export_desc);
+    // cleanup
     doca_dma_destroy(dma);
     doca_dev_close(dev);
-    doca_pe_destroy(pe);
-    doca_comch_ep_close(dpu_ep);
+    doca_mmap_destroy(host_mmap);
+    doca_buffer_destroy(buf_host_remote);
+    doca_buffer_destroy(buf_dpu);
+    doca_mmap_destroy(dpu_mmap);
+    free(recv_desc);
+    free(dpu_buf);
+    close(sock);
 
     return 0;
 }
--- dma_transfer/dma_copy_host.c
+++ dma_transfer/dma_copy_host.c
@@ -1,56 +1,129 @@
-#include <doca_mmap.h>
-#include <doca_dma.h>
-#include <doca_comch.h>
+// dma_host.c
+// Build with DOCA 2.7 libs. Host side: allocate destination buffer, export via doca_mmap,
+// listen for DPU connection, send export descriptor & metadata, wait for "DONE".
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <fcntl.h>
 
-#include "dma_copy.h"
+#include <doca_mmap.h>
+#include <doca_buffer.h>
+#include <doca_error.h>
+
+#define PORT 8888
+#define BUF_SIZE 4096     // default
+#define EXPORT_MAX 65536  // max export descriptor size we expect
+
+static void die(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv) {
-    // 1. Parse args: file name, size, PCIe address of comm channel (‐p), etc
-    // 2. init DOCA comm channel endpoint
-    doca_comch_ep_t *host_ep = NULL;
-    char *pcie_addr = getenv("PCIe_ADDR");
-    doca_comch_ep_open(pci, &host_ep);
+    int listen_fd = -1, client_fd = -1;
+    void *host_buf = NULL;
+    size_t buf_size = BUF_SIZE;
 
-    // 3. Allocate host buffer (receive buffer)
-    size_t buf_size = BUFSIZE;
-    void *host_buf = aligned_alloc(sysconf(_SC_PAGESIZE), buf_size);
-    if (!host_buf) { perror("malloc"); return -1; }
+    // Create host destination buffer (page-aligned)
+    if (posix_memalign(&host_buf, sysconf(_SC_PAGESIZE), buf_size) != 0) {
+        die("posix_memalign");
+    }
+    memset(host_buf, 0x0, buf_size);
 
-    // 4. Export host buffer for DPU to map  
+    // Create DOCA mmap for the host buffer and export it for PCI
     doca_mmap_t *host_mmap = NULL;
-    doca_mmap_create(&host_mmap);
+    if (doca_mmap_create(&host_mmap) != 0) {
+        fprintf(stderr, "doca_mmap_create failed\n");
+        return -1;
+    }
     doca_mmap_set_memrange(host_mmap, host_buf, buf_size);
     doca_mmap_set_permissions(host_mmap, DOCA_ACCESS_FLAG_PCI_READ_WRITE);
-    doca_mmap_start(host_mmap);
-
-    uint8_t *export_desc = NULL;
-    size_t export_desc_size = 0;
-    doca_mmap_export_pci(host_mmap, /* dev pointer */,
-                          &export_desc, &export_desc_size);
-
-    // 5. Send export descriptor + host buffer address + size over comm channel
-    doca_comch_send(host_ep, export_desc, export_desc_size, 0);
-    doca_comch_send(host_ep, &host_buf, sizeof(host_buf), 0);
-    doca_comch_send(host_ep, &buf_size, sizeof(buf_size), 0);
-
-    // 6. Wait for DPU to signal completion
-    int done = 0;
-    doca_comch_recv(host_ep, &done, sizeof(done), /*timeout*/);
-    if (done != 1) {
-        fprintf(stderr, "DPU signalled failure\n");
+    if (doca_mmap_start(host_mmap) != 0) {
+        fprintf(stderr, "doca_mmap_start failed\n");
         return -1;
     }
 
-    // 7. At this point host_buf contains data from DPU — process it
-    process_data(host_buf, buf_size);
+    // Export mmap for PCI (produce export descriptor bytes)
+    uint8_t *export_desc = malloc(EXPORT_MAX);
+    size_t export_desc_len = EXPORT_MAX;
+    int rc = doca_mmap_export_pci(host_mmap, export_desc, &export_desc_len);
+    if (rc != 0) {
+        fprintf(stderr, "doca_mmap_export_pci failed: %d\n", rc);
+        return -1;
+    }
+    printf("Host: exported mmap, desc len=%zu bytes\n", export_desc_len);
 
-    // 8. Clean up
+    // Setup TCP server
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) die("socket");
+
+    int opt = 1;
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) die("setsockopt");
+
+    struct sockaddr_in servaddr = {0};
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY; // listen on all interfaces; restrict as needed
+    servaddr.sin_port = htons(PORT);
+
+    if (bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) die("bind");
+    if (listen(listen_fd, 1) < 0) die("listen");
+    printf("Host: listening on port %d\n", PORT);
+
+    client_fd = accept(listen_fd, NULL, NULL);
+    if (client_fd < 0) die("accept");
+    printf("Host: DPU connected\n");
+
+    // Send metadata: (1) export_desc_len (uint32_t network), (2) export_desc bytes, (3) host virtual pointer (uint64_t), (4) buf_size (uint64_t)
+    uint32_t net_desc_len = htonl((uint32_t)export_desc_len);
+    if (send(client_fd, &net_desc_len, sizeof(net_desc_len), 0) != sizeof(net_desc_len)) die("send desc len");
+
+    ssize_t s = send(client_fd, export_desc, export_desc_len, 0);
+    if (s != (ssize_t)export_desc_len) die("send export desc");
+
+    // send the host buffer virtual address (so remote can offset into exported region)
+    uint64_t host_ptr = (uint64_t)host_buf;
+    uint64_t host_ptr_net = htobe64(host_ptr);
+    if (send(client_fd, &host_ptr_net, sizeof(host_ptr_net), 0) != sizeof(host_ptr_net)) die("send host ptr");
+
+    uint64_t size_net = htobe64((uint64_t)buf_size);
+    if (send(client_fd, &size_net, sizeof(size_net), 0) != sizeof(size_net)) die("send buf size");
+
+    printf("Host: sent export descriptor + metadata to DPU\n");
+
+    // Wait for "DONE" message (4 bytes)
+    char done_buf[8] = {0};
+    ssize_t r = recv(client_fd, done_buf, sizeof(done_buf), 0);
+    if (r <= 0) {
+        fprintf(stderr, "Host: recv() failed or connection closed\n");
+    } else {
+        if (strncmp(done_buf, "DONE", 4) == 0) {
+            printf("Host: Received DONE from DPU. Data is now in host buffer:\n");
+            // For demo, hexdump first 64 bytes
+            unsigned char *p = (unsigned char *)host_buf;
+            for (int i = 0; i < (int) (buf_size > 64 ? 64 : buf_size); ++i) {
+                if (i % 16 == 0) printf("\n%04x: ", i);
+                printf("%02x ", p[i]);
+            }
+            printf("\n");
+        } else {
+            printf("Host: Received unexpected message: %s\n", done_buf);
+        }
+    }
+
+    // cleanup
+    close(client_fd);
+    close(listen_fd);
     doca_mmap_destroy(host_mmap);
+    free(export_desc);
     free(host_buf);
-    doca_comch_ep_close(host_ep);
 
     return 0;
 }
